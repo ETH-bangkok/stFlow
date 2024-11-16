50c0cf2d0ebf74f504f4570965ee37711389672c
manu <manujasan23@gmail.com>
2024-11-16 19:47:45 +0700

    feat: beforeswaphook for steth to eth

--- packages/foundry/contracts/stFlowContract.sol
+++ packages/foundry/contracts/stFlowContract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import { BaseHook } from "v4-periphery/src/base/hooks/BaseHook.sol";
-
+import { Currency } from "v4-core/src/types/Currency.sol";
 import { Hooks } from "v4-core/src/libraries/Hooks.sol";
 import { IPoolManager } from "v4-core/src/interfaces/IPoolManager.sol";
 import { PoolKey } from "v4-core/src/types/PoolKey.sol";
@@ -10,11 +10,29 @@ import { PoolId, PoolIdLibrary } from "v4-core/src/types/PoolId.sol";
 import { BalanceDelta } from "v4-core/src/types/BalanceDelta.sol";
 import {
   BeforeSwapDelta,
-  BeforeSwapDeltaLibrary
+  BeforeSwapDeltaLibrary,
+  toBeforeSwapDelta
 } from "v4-core/src/types/BeforeSwapDelta.sol";
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "v3-periphery/contracts/libraries/TransferHelper.sol";
+import { ISwapRouter } from "v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+interface Lido {
+  function wrap(uint256 _stETHAmount) external returns (uint256);
+}
+
+interface WETH {
+  function deposit() external payable;
+  function withdraw(uint256 wad) external;
+}
 
 contract StFlowHook is BaseHook {
   using PoolIdLibrary for PoolKey;
+
+  Lido steth_wrapper;
+  address immutable wsteth;
+  ISwapRouter immutable v3_swapRouter;
+  WETH immutable weth;
 
   // NOTE: ---------------------------------------------------------
   // state variables should typically be unique to a pool
@@ -28,8 +46,17 @@ contract StFlowHook is BaseHook {
   mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
 
   constructor(
-    IPoolManager _poolManager
-  ) BaseHook(_poolManager) { }
+    IPoolManager _poolManager,
+    Lido steth_wrapper_,
+    address wsteth_,
+    ISwapRouter v3_swapRouter_,
+    WETH weth_contract
+  ) BaseHook(_poolManager) {
+    steth_wrapper = steth_wrapper_;
+    wsteth = wsteth_;
+    v3_swapRouter = v3_swapRouter_;
+    weth = weth_contract;
+  }
 
   function getHookPermissions()
     public
@@ -40,15 +67,15 @@ contract StFlowHook is BaseHook {
     return Hooks.Permissions({
       beforeInitialize: false,
       afterInitialize: false,
-      beforeAddLiquidity: true,
+      beforeAddLiquidity: false,
       afterAddLiquidity: false,
-      beforeRemoveLiquidity: true,
+      beforeRemoveLiquidity: false,
       afterRemoveLiquidity: false,
       beforeSwap: true,
-      afterSwap: true,
+      afterSwap: false,
       beforeDonate: false,
       afterDonate: false,
-      beforeSwapReturnDelta: false,
+      beforeSwapReturnDelta: true,
       afterSwapReturnDelta: false,
       afterAddLiquidityReturnDelta: false,
       afterRemoveLiquidityReturnDelta: false
@@ -62,41 +89,57 @@ contract StFlowHook is BaseHook {
   function beforeSwap(
     address,
     PoolKey calldata key,
-    IPoolManager.SwapParams calldata,
+    IPoolManager.SwapParams calldata params,
     bytes calldata
   ) external override returns (bytes4, BeforeSwapDelta, uint24) {
-    beforeSwapCount[key.toId()]++;
-    return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
-  }
+    BeforeSwapDelta beforeSwapDelta;
 
-  function afterSwap(
-    address,
-    PoolKey calldata key,
-    IPoolManager.SwapParams calldata,
-    BalanceDelta,
-    bytes calldata
-  ) external override returns (bytes4, int128) {
-    afterSwapCount[key.toId()]++;
-    return (BaseHook.afterSwap.selector, 0);
-  }
+    if (!params.zeroForOne) {
+      //Steth for ETH
+      //go to lido, swap all the steth tokens with wsteth
 
-  function beforeAddLiquidity(
-    address,
-    PoolKey calldata key,
-    IPoolManager.ModifyLiquidityParams calldata,
-    bytes calldata
-  ) external override returns (bytes4) {
-    beforeAddLiquidityCount[key.toId()]++;
-    return BaseHook.beforeAddLiquidity.selector;
-  }
+      uint256 amountInOutPositive = params.amountSpecified > 0
+        ? uint256(params.amountSpecified)
+        : uint256(-params.amountSpecified);
 
-  function beforeRemoveLiquidity(
-    address,
-    PoolKey calldata key,
-    IPoolManager.ModifyLiquidityParams calldata,
-    bytes calldata
-  ) external override returns (bytes4) {
-    beforeRemoveLiquidityCount[key.toId()]++;
-    return BaseHook.beforeRemoveLiquidity.selector;
+      poolManager.take(key.currency0, address(this), amountInOutPositive);
+
+      IERC20(Currency.unwrap(key.currency0)).approve(
+        address(steth_wrapper), amountInOutPositive
+      );
+
+      steth_wrapper.wrap(amountInOutPositive);
+
+      TransferHelper.safeApprove(
+        wsteth, address(v3_swapRouter), amountInOutPositive
+      );
+
+      ISwapRouter.ExactInputSingleParams memory swap_params = ISwapRouter
+        .ExactInputSingleParams({
+        tokenIn: wsteth,
+        tokenOut: address(0),
+        fee: 500, //edit: fee
+        recipient: address(this),
+        deadline: block.timestamp,
+        amountIn: amountInOutPositive,
+        amountOutMinimum: 0,
+        sqrtPriceLimitX96: 0
+      });
+
+      uint256 amountOut = v3_swapRouter.exactInputSingle(swap_params);
+
+      weth.withdraw(amountOut);
+
+      beforeSwapDelta = toBeforeSwapDelta(
+        int128(-params.amountSpecified), // So `specifiedAmount` = +100
+        int128(-int128(uint128(amountOut))) // Unspecified amount (output delta) = -100
+      );
+
+      poolManager.settle();
+    } else {
+      //Eth for Steth
+    }
+
+    return (this.beforeSwap.selector, beforeSwapDelta, 0);
   }
 }
